afde94f70f715c84a996f37e6ce7028c6f55e9df
allan zizinga <alla@noq.nu>
2025-04-14 22:29:39 +0200

    Added contact variables to env file

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -3,20 +3,21 @@ import { RevealOnScroll } from "../RevealOnScroll"
 import emailjs from 'emailjs-com';
 
 export const Contact = ()=>{
-    const [form, setFormData] = useState({
+    const [formData, setFormData] = useState({
         name: "",
         email: "",
-        message: ""
+        message: "",
     })
    
 
-    const SERVICE_ID = "service_xp9n7ev"
-    const TEMPLATE_ID ="template_fmvu9zs"
-    const PUBLIC_KEY ="QQ4oZkawqZC9teova"
+    // const SERVICE_ID = "service_xp9n7ev"
+    // const TEMPLATE_ID ="template_fmvu9zs"
+    // const PUBLIC_KEY ="QQ4oZkawqZC9teova"
 
     const handleSubmit = (e)=>{
         e.preventDefault()
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY).then((result)=>{
+
+        emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY).then((result)=>{
             alert("Message sent successfully")
             setFormData({name: "", email: "", message:""})
         }).catch(()=>{
@@ -33,14 +34,14 @@ export const Contact = ()=>{
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to cyan-400 bg-clip-text text-transparent text-center"> Get Intouch</h2>
             <form className="space-y-6" onSubmit={handleSubmit}>
                    <div className="relative">
-                     <input type="text" id="name" required value={FormData.name} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "  placeholder="Your Name"  onChange={((e)=>setFormData({...formData, [e.target.name]: e.target.value}))} />
+                     <input type="text" id="name" required value={formData.name} className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "  placeholder="Your Name"  onChange={((e)=> setFormData({...formData, [e.target.name]: e.target.value}))} />
                    </div>
                    <div className="relative">
-                     <input type="email" id="email" name="email" value={FormData.email}   required className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "  placeholder="example@yahoo.com" onChange={((e)=> setFormData({...formData, [e.target.email]: e.target.value}))} />
+                     <input type="email" id="email" name="email" value={formData.email}   required className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "  placeholder="example@yahoo.com" onChange={((e)=> setFormData({...formData, [e.target.email]: e.target.value}))} />
                    </div>
 
                    <div className="relative">
-                     <textarea  id="message" name="message" value={FormData.message}  rows={6} required className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "  placeholder="I am contacting you about..."  onChange={((e)=> setFormData({...formData,  [e.target.message]: e.target.value}))}/>
+                     <textarea  id="message" name="message" value={formData.message}  rows={6} required className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5 "  placeholder="I am contacting you about..."  onChange={((e)=> setFormData({...formData,  [e.target.message]: e.target.value}))}/>
                    </div>
                    <button className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">Submit</button>
 
