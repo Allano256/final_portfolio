7c26dac54c6d90bae87ef3664f2a21da6b4f67b5
allan zizinga <alla@noq.nu>
2025-05-18 00:47:40 +0200

    Updated css on restaurant app

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -17,7 +17,7 @@ export const Projects =()=>{
             
             <p className="text-gray-400 mb-4"> A restaurant booking application that allows customers make reservations at the restaurant.</p>
             <div className="flex flex-wrap gap-2 mb-4">
-                {["Django","Python", "Postgres", "Django Rest Framework API"].map((skill, key)=>(
+                {["Django","Python", "Postgres","CSS", "Django Rest Framework API"].map((skill, key)=>(
                     <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition-all" >
                          {skill}
                     </span>
