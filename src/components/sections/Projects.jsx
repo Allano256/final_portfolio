804d1c170c47651387c6a3e2d514c04716e97f83
allan zizinga <alla@noq.nu>
2025-05-18 00:41:28 +0200

    Updated links to projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -25,7 +25,27 @@ export const Projects =()=>{
             </div>
 
             <div className="flex justify-between items-center">
-                <a href="" className="text-blue-400 hover:text-blue-300 transition-color">View Project ➡️</a>
+                <a href="https://github.com/Allano256/restaurant_app" className="text-blue-400 hover:text-blue-300 transition-color">View Project  </a>
+            </div>
+
+         </div>
+
+
+         {/* Restaurant application */}
+            <div className="rounded-xl p-6 border-white/10 border hover:-translate-y-1  hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all " >
+                <h3 className="text-xl font-bold mb-2">Foodshare Application</h3>
+            
+            <p className="text-gray-400 mb-4"> An application that helps people/companies share surplus food..</p>
+            <div className="flex flex-wrap gap-2 mb-4">
+                {["React","Javascript","TailwindCSS","HTML"].map((skill, key)=>(
+                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition-all" >
+                         {skill}
+                    </span>
+                ))}
+            </div>
+
+            <div className="flex justify-between items-center">
+                <a href="https://foodshareconect.netlify.app/" className="text-blue-400 hover:text-blue-300 transition-color">View Project  </a>
             </div>
 
          </div>
@@ -44,7 +64,7 @@ export const Projects =()=>{
             </div>
 
             <div className="flex justify-between items-center">
-                <a href="" className="text-blue-400 hover:text-blue-300 transition-color">View Project ➡️</a>
+                <a href="https://yourbmichecker.netlify.app/" className="text-blue-400 hover:text-blue-300 transition-color">View Project  </a>
             </div>
 
          </div>
